f6e342607eeac65c1540dde421db80466451c791
alvaro cortes <frijoles4me@gmail.com>
2018-08-29 01:48:00 -0700

    adding passport

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://admin:res6nvhg@ds125892.mlab.com:25892/gamerz",
+  googleClientID:
+    "925853906326-2fho7qft199dn1t0nob6d22md9qotc3v.apps.googleusercontent.com",
+  googleClientSecret: "Xsy_C_unOXs1dNU1oMEjsoD3"
+};
