563aa67f7c1393953143e3d5d598db104cb2a42e
alvaro cortes <frijoles4me@gmail.com>
2018-09-04 00:50:07 -0700

    passport working

--- config/passport.js
+++ config/passport.js
@@ -5,15 +5,17 @@ const keys = require("./keys");
 
 module.exports = function(passport) {
   passport.use(
-    new GoogleStrategy({
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "http://localhost:5000/auth/google/callback",
-      proxy: true
-    })
-  ),
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(profile);
-    };
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: "/auth/google/callback",
+        proxy: true
+      },
+      (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken);
+        console.log(profile);
+      }
+    )
+  );
 };
