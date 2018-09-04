0691ea41565d589159b50a9306755e8baa3002c4
alvaro cortes <frijoles4me@gmail.com>
2018-09-04 01:11:55 -0700

    added the callback route

--- routes/auth.js
+++ routes/auth.js
@@ -7,4 +7,14 @@ router.get(
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
+//
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // Successful authentication, redirect dashboard.
+    res.redirect("/dashboard");
+  }
+);
+
 module.exports = router;
